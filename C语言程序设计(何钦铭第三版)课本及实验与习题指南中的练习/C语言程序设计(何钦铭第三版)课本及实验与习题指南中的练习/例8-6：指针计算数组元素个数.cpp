03acb0079f228733654472472368de70a3524491
SuperCoder1999 <331150871@qq.com>
2021-04-06 22:16:47 +0800

    1

---
+++ C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例8-6：指针计算数组元素个数.cpp
@@ -0,0 +1,12 @@
+//// 首先这题的应用场景不清楚，目前我忘记鹏哥视频里教的。所以留待
+//#include"Head.h"
+//#include<cstdio>
+//
+//int main() {
+//	double a[2], * p, * q;
+//	p = &a[0];
+//	q = p + 1;
+//	printf("%d\n", q - p); // 计算指针间元素个数
+//	printf("%d", (int)q - (int)p); // 计算指针间字节数
+//	return 0;
+//}
