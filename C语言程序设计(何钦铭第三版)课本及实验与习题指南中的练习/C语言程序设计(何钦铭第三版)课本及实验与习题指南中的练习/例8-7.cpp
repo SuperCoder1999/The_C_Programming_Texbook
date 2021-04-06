7b506732c204c6a0667c643f7ce878d91b571a14
SuperCoder1999 <331150871@qq.com>
2021-04-06 22:44:15 +0800

    1

---
+++ C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例8-7.cpp
@@ -0,0 +1,16 @@
+//#include"Head.h"
+//#include<cstdio>
+//int main() {
+//	int arr[10] = { 0 };
+//	for (int i = 0; i < 10; i++)
+//		cin >> arr[i];
+//
+//	int sum1 = 0, sum2 = 0;
+//	for (int j = 0; j < 10; j++)
+//		sum1 += arr[j];
+//	for (int k = 0; k < 10; k++)
+//		sum2 += *(arr + k);// 这里也可以定义一个指针p，指向arr[0]，p++来寻找元素
+//	// 解引用运算符 》 算术运算符 》关系运算符 》逻辑运算符 》赋值运算符
+//	cout << sum1 << " " << sum2;
+//	return 0;
+//}
