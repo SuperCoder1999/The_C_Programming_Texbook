01139cc615e8831302a1aa76d5e68f2f8636e411
SuperCoder1999 <331150871@qq.com>
2021-04-09 20:27:19 +0800

    1

--- C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例8-8：二分查找.cpp
+++ C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例8-8：二分查找.cpp
@@ -21,3 +21,23 @@
 //	cout << "Not found";
 //	return 0;
 //}
+
+#include"Head.h"
+#include<cstdio>
+
+int Bsearch(int arr[], int x) {
+	int left = 0, right = 10;
+	int mid = (left + rught) / 2;
+
+}
+
+int main() {
+	int arr[10] = {0, 1,2,3,4,5,6,7,8,9 };
+	int x = 0;
+	cout << "Enter x:";
+	cin >> x;
+
+	int loc=Bsearch(arr, x);
+	cout << loc;
+	return 0;
+}
