2b5a72e6a907548deaac48c3cc8b7c82e2f1a606
SuperCoder1999 <331150871@qq.com>
2021-04-13 23:13:26 +0800

    1

--- C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例11-6.cpp
+++ C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例11-6.cpp
@@ -12,15 +12,17 @@
 //	scanf("%s", tmp);
 //	// char** p; 这里二级指针几乎没用，原题中也是用的color[i]，并没有用到color(二级指针)。
 //	// 指针没有初始化，不能使用
-//	char** p=NULL;
-//	while (tmp[20] != '#') {
+//	// 指针的初始化，必须要有变量、数组才行，这么来看，直接定义数组方便得多
+//	char* string[20];
+//	char** p=string;
+//	while (tmp[0] != '#') {// 这里tmp字符串内还有 \0 结尾，所以不能和'#'比较
 //		*(p+count) = (char*)malloc(20);
-//		strcpy(*p, tmp);
+//		strcpy(*(p+count), tmp);
 //		count++;
 //		scanf("%s", tmp);
 //	}
 //	for (int i = 0; i < count; i++) {
-//		printf("%s  ", *(p + count));
+//		printf("%s  ", *(p + i));// 二级指针 应用时 * 解引用之后得到的是什么。这个思维还不是很熟练
 //	}
 //	return 0;
 //}
