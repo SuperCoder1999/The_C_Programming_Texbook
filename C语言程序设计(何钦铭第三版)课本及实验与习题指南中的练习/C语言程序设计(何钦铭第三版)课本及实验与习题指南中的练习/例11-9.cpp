e0be5bd62a1237cf2d41f6738ab0740e64de43b4
SuperCoder1999 <331150871@qq.com>
2021-04-14 22:54:35 +0800

    1

---
+++ C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例11-9.cpp
@@ -0,0 +1,31 @@
+///*
+//double calc(double (*funp) (double), double a, double b);
+// 这里calc 函数引用的函数 是省略了其中的变量名 
+//*/
+//// double calc(double (*fun)(double), double a, double b) 必须是(*fun)形式
+//#include"Head.h"
+//#include<cstdio>
+//#include<math.h>
+//
+//
+//
+//double fun1(double x) {
+//	return x * x;
+//}
+//double fun2(double x) {
+//	return sin(x) / x;
+//}
+//double calc(double (*fun)(double), double a, double b) {
+//	return (b - a) / 2 * ((*fun)(a) + (*fun)(b));
+//}
+//int main() {
+//	double a = 0, b = 0;
+//	double(*fun)(double);// 定义的函数指针，用于调用不同函数
+//	cout << "Enter a,b: ";
+//	cin >> a >> b;
+//	fun = fun1;
+//	cout << calc(fun, a, b);
+//	fun = fun2;
+//	cout << calc(fun, a, b);
+//	return 0;
+//}
