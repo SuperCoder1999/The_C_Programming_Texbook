19b4d713215e2bbecd21e6ff32bcff18149f2ba6
SuperCoder1999 <331150871@qq.com>
2022-01-04 10:07:12 +0800

    1

--- C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例2-4.cpp
+++ C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/C语言程序设计(何钦铭第三版)课本及实验与习题指南中的练习/例2-4.cpp
@@ -1,15 +1,15 @@
-//#include"Head.h"
-//#include<cstdio>
-//
-//int main() {
-//	float x = 0;
-//	float y = 0;
-//	printf("Enter x (x>=0):\n");
-//	scanf("%f", &x);
-//	if (x <= 15)
-//		y = 4 * x / 3;
-//	else
-//		y=2.5 * x - 10.5;
-//	printf("y=f(%f)=%.2f",x, y);
-//	return 0;
-//}
+#include"Head.h"
+#include<cstdio>
+
+int main() {
+	float x = 0;
+	float y = 0;
+	printf("Enter x (x>=0):\n");
+	scanf("%f", &x);
+	if (x <= 15)
+		y = 4 * x / 3;
+	else
+		y=2.5 * x - 10.5;
+	printf("y=f(%f)=%.2f",x, y);
+	return 0;
+}
